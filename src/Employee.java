73be7df2e6bf9cfeee511c5861789aa3e72f4984
verzhik <verzhik11@gmail.com>
2023-04-08 13:54:48 +0300

    Done

---
+++ src/Employee.java
@@ -0,0 +1,41 @@
+import java.util.Objects;
+
+public class Employee {
+    private final String name;
+    private final String lastname;
+
+
+    public Employee(String name, String lastname) {
+        this.name = name;
+        this.lastname = lastname;
+    }
+
+    public String toString() {
+        return "Работник" + " " + this.name + " " + this.lastname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getFullName() {
+        return name + " " + lastname;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return name.equals(employee.name) && lastname.equals(employee.lastname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, lastname);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
+
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        EmployeeBook employeeBook = new EmployeeBook();
+        Map<String, Employee> emp = employeeBook.getEmployees();
+        employeeBook.addEmployee(new Employee("Wolf", "Messing"));
+        employeeBook.addEmployee(new Employee("Brad", "Pitt"));
+        employeeBook.addEmployee(new Employee("Alan", "Po"));
+        //employeeBook.removeEmployee(new Employee("Brad", "Pitt"));
+        employeeBook.showAll();
+        //employeeBook.findEmployee(new Employee("Alan", "Po"));
+
     }
+
+
+
+
+    //        Map<String, Employee> employees = new HashMap<>(Map.of(
+//                "teamLeader", new Employee("Wolf", "Messing"),
+//                "senior", new Employee("Brad", "Pitt"),
+//                "junior1", new Employee("Alan", "Po"),
+//                "junior2", new Employee("Richard", "Wagner")
+//        ));
 }
